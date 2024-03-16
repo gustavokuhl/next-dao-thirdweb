415f296e63af6aadb2c3253e4c4a007bdebcebbb
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-03-16 20:51:00 -0300

    feat: add connect wallet buttom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { ConnectButton } from "./thirdweb"
+
 export default function Home() {
-  return <main></main>
+  return (
+    <main className="w-full h-screen flex justify-center items-center">
+      <ConnectButton />
+    </main>
+  )
 }
